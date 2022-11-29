86caab6cc6510c6929970511c9290d58a5e3dc62
Jacob Michels <jacob.michels2025@gmail.com>
2022-11-28 19:14:28 -0500

    fix newly created grid spaces having bad values

--- web/src/GridParameters.tsx
+++ web/src/GridParameters.tsx
@@ -10,7 +10,7 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { colsCount, gridState, rowsCount } from "./App";
+import { colsCount, GridCell, gridState, rowsCount } from "./App";
 
 export const GridParameters: React.FC = () => {
   const [, gridSetter] = useRecoilState(gridState);
@@ -27,10 +27,12 @@ export const GridParameters: React.FC = () => {
           value={rows}
           onChange={(value) => {
             gridSetter((oldGrid) => {
-              let newGrid: string[][] = JSON.parse(JSON.stringify(oldGrid));
+              let newGrid: GridCell[][] = JSON.parse(JSON.stringify(oldGrid));
               if (newGrid.length < parseInt(value)) {
                 for (let i = newGrid.length; i < parseInt(value); i++) {
-                  newGrid.push(Array(cols).fill("BD"));
+                  newGrid.push(
+                    Array(cols).fill({ value: "BD", isHighlighted: false })
+                  );
                 }
               } else if (newGrid.length > parseInt(value)) {
                 newGrid = newGrid.slice(0, parseInt(value));
@@ -54,11 +56,11 @@ export const GridParameters: React.FC = () => {
           value={cols}
           onChange={(value) => {
             gridSetter((oldGrid) => {
-              let newGrid: string[][] = JSON.parse(JSON.stringify(oldGrid));
+              let newGrid: GridCell[][] = JSON.parse(JSON.stringify(oldGrid));
               for (let i = 0; i < newGrid.length; i++) {
                 if (newGrid[i].length < parseInt(value)) {
                   for (let j = newGrid[i].length; j < parseInt(value); j++) {
-                    newGrid[i].push("BD");
+                    newGrid[i].push({ value: "BD", isHighlighted: false });
                   }
                 } else if (newGrid[i].length > parseInt(value)) {
                   newGrid[i] = newGrid[i].slice(0, parseInt(value));
